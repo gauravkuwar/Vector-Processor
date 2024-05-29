ee6d351c616af12cd4971b8129bfe3832e1ad2f8
Gaurav Kuwar <gauravk@Gauravs-MacBook-Pro.local>
2024-05-29 11:57:13 -0400

    added container lib, which is an immutable set, and can efficiently check if some value is in set

--- c/functional_simulator/main.c
+++ c/functional_simulator/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-// #include "../helpers/set.h"
+#include "../helpers/container.h"
 // #include "instrs_sets.h"
 
 // 32 bit words
@@ -13,18 +13,9 @@
 #define MAX_INSTR_LINE_LEN 23 // max len of instr name + op1 len + op2 len + op3 len + spaces + extra = 6 + 4 + 4 + 4 + 3 + 2
 #define MAX_SUB_STR_LEN 8
 
-// helper functions
+// helper functions; TODO: move to different file
 void print_usage() {
     printf("usage: ./funcsim [iodir]\n");
-}
-
-bool contains(char *val, char *array[], int size) {
-    // check if val is in array
-    for (int i=0; i<size; i++) {
-        if (array[i] == val)
-            return true;
-    }
-    return false;
 }
 
 bool str_is_null(char *s) {
@@ -34,6 +25,7 @@ bool str_is_null(char *s) {
 void str_set_null(char *s) {
     s[0] = '\0';
 }
+
 
 // memory
 int32_t vdmem[VDMEM_SIZE];
@@ -69,6 +61,7 @@ struct Instr {
     char op1[MAX_SUB_STR_LEN];
     char op2[MAX_SUB_STR_LEN];
     char op3[MAX_SUB_STR_LEN];
+    int line_num;
 } Instr;
 
 
@@ -88,52 +81,17 @@ int get_num_of_instrs(FILE *asm_file) {
 }
 
 
-int fetch_decode(char *iodir) {
-    const char *code_asm = "/Code.asm";
-    char asm_fp[strlen(iodir) + strlen(code_asm)];
-
-    char buf[1024];
-    int num_of_instrs = 0;
-
+int fetch_decode(FILE *asm_file, struct Instr *instrs, int num_of_instrs) {
     char char_buf;
     char str_buf[MAX_SUB_STR_LEN];
-    
     int cidx = 0; // counter for character for str_buf
     int sidx = 0; // counter keep track of substrings
     int iidx = 0; // counter for instruction for instrs
-
-    // Derive asm file path
-    strcpy(asm_fp, iodir);
-    strcat(asm_fp, "/Code.asm");
-
-    FILE *asm_file = fopen(asm_fp, "r");
-    if (asm_file == NULL) {
-        perror("Error opening Code.asm file");
-        return -1;
-    }
-    
-    // get number of instructions in file
-    while (fgets(buf, sizeof(buf), asm_file) != NULL) {
-        // Check if the line is not empty and not a comment
-        if ((buf[0] != '\n' && buf[0] != '\0' && buf[0] != '#'))
-            num_of_instrs++;
-    }
-    fseek(asm_file, 0, SEEK_SET);
-    printf("Num of instr = %d\n", num_of_instrs);
-    
     
     // Read Code.asm, filter out comments and store as Instr struct in instrs array
-    struct Instr instrs[num_of_instrs]; // array of instructions
-    // initialize all strings in struct to null
-    for (int i=0; i<num_of_instrs; i++) {
-        str_set_null(instrs[i].name);
-        str_set_null(instrs[i].op1);
-        str_set_null(instrs[i].op2);
-        str_set_null(instrs[i].op3);
-    }
-
     // The loop decodes instruction and splits them into name and operands
     // and stores in instrs array
+
     do {
         char_buf = fgetc(asm_file);
         if (char_buf == '#') // skip that line because its comment
@@ -166,23 +124,35 @@ int fetch_decode(char *iodir) {
         }
         
         if (char_buf == '\n' || char_buf == EOF) {
-            if (!str_is_null(instrs[iidx].name)) {
-                // verify if instruction is valid else return error
-                // check if its in set of all instructions
-                // if (instrs[iidx].name not in set_of_all_instrs) {
-                //     return -1
-                // }
+            if (!str_is_null(instrs[iidx].name))
                 iidx++;
-            }
 
             cidx = 0;
             sidx = 0;
         }
     } while (char_buf != EOF);
 
-    // printf("%s, %s, %s, %s\n", instrs[iidx].name, instrs[iidx].op1, instrs[iidx].op2, instrs[iidx].op3);
+    // fseek(asm_file, 0, SEEK_SET);
+    return 0;
+}
 
-    fclose(asm_file);
+int verify(struct Instr *instrs, int num_of_instrs) {
+    // verify if all instructions are valid else return error
+    for (int i=0; i<num_of_instrs; i++) {
+        // check if its in set of all instructions
+        // if (!contains(instrs[i].name, all_instrs)) {
+        //     fprintf(stderr, "Error: invalid \n"); // need line number
+        //     return -1;
+        // }
+        
+        // check if each instruction has correct num of operands
+        // int num_of_operands = !str_is_null(instrs[i].op1) + !str_is_null(instrs[i].op2) + !str_is_null(instrs[i].op3);
+        // map instrs[i].name -> correct_num_of_operands
+        // if (instrs[i].name)
+        
+        // check if each operand is of correct type: scalar, vector, immediate
+        // map instrs[i].op[0] == instrs[i]
+    }
     return 0;
 }
 
@@ -204,6 +174,8 @@ int main(int argc, char *argv[]) {
 
 
     char *iodir = argv[1];
+    // TODO: check if iodir is valid path
+
     const char *code_asm = "/Code.asm";
     char asm_fp[strlen(iodir) + strlen(code_asm)];
 
@@ -214,39 +186,28 @@ int main(int argc, char *argv[]) {
     FILE *asm_file = fopen(asm_fp, "r");
     if (asm_file == NULL) {
         perror("Error opening Code.asm file");
-        return -1;
+        return 1;
     }
 
     int num_of_instrs = get_num_of_instrs(asm_file);
-    fetch_decode(iodir);
+    struct Instr instrs[num_of_instrs]; // array of instructions
+    // initialize all strings in struct to null
+    for (int i=0; i<num_of_instrs; i++) {
+        str_set_null(instrs[i].name);
+        str_set_null(instrs[i].op1);
+        str_set_null(instrs[i].op2);
+        str_set_null(instrs[i].op3);
+    }
 
+    fetch_decode(asm_file, instrs, num_of_instrs);
+    fclose(asm_file);
+    
+    // verify correctness of assembly code
+    verify(instrs, num_of_instrs);
 
-
-    // char str[] = "INST OP1 OP2 # comment";
-    // const char delim[] = " "; // Delimiter
-
-    // // Use strtok to split the string
-    // char *token = strtok(str, " ");
-    // char *substrs[4];
-    // int idx = 0;
-
-    // // Loop through the tokens
-    // while (idx < 4 && token != NULL) {
-    //     // printf("%s\n", token);
-    //     substrs[idx++] = token;
-    //     token = strtok(NULL, delim);
+    // for (int i=0; i<num_of_instrs; i++) {
+    //     printf("%s %s %s %s\n", instrs[i].name, instrs[i].op1, instrs[i].op2, instrs[i].op3);
     // }
-
-    // for (int i=0; i<4; i++)
-    //     printf("%s\n", substrs[i]); 
-
-    // TODO: check if iodir is valid path
-
-    // int num_of_instrs;
-    // char *all_instrs[num_of_instrs];
-
-    // // fetch and verify instrs from assembly file
-    // // decode instrs
 
     // init_vec_mask_reg();
     // int PC = 0;
